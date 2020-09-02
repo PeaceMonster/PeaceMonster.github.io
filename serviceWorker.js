adbe275a509c5a7180c5d45ff00dceff5e0d1d41
PeaceMonster <43810012+PeaceMonster@users.noreply.github.com>
2020-09-02 21:42:44 +0200

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,25 @@
+const version = 1;
+const cacheName = `test-version-${version}`;
+
 self.addEventListener("install", event => {
     console.log("SW installed");
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll([
+                "/index.html",
+                "/main.js",
+                "style.css"
+            ]);
+        })
+    )
+})
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.open(cacheName).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request.url);
+            })
+        })
+    )
 })
